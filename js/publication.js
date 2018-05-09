00a121ba4e792d5ec66e623edbe491a4473f6349
hanchung <ghoo1125@gmail.com>
2018-05-09 13:58:28 +0800

    Make underlines animation smoother.

--- js/publication.js
+++ js/publication.js
@@ -8,37 +8,70 @@ let current = 0;
   // init to conference category
   underlines[current].style.visibility = "visible";
 
+  // there is only one category
+  if (categories.length < 2) {
+    return;
+  }
+
   // add event listener to each category
   for (let i = 0; i < categories.length; i++) {
     categories[i].firstChild.addEventListener("click", function() {
-      for (let j = 0; j < categories.length; j++) {
-        if (i == j) {
-          // replace node to restart animation
-          let newone = underlines[j].cloneNode(true);
-          underlines[j].parentNode.replaceChild(newone, underlines[j]);
-          newone.style.visibility = "visible";
-
-          // set up animation keyframes
-          let pos = current < i ? -300 : 300;
-          let str = "translateX(" + pos + "px)";
-          newone.animate([
-            {  // from
-              transform: str,
-              opacity: 0,
-            },
-            {  // to
-              transform: 'translateX(0px)',
-              opacity: 1,
-            }
-          ], {
-            duration: 500,
-          });
-
-          current = i;
-        } else {
-          underlines[j].style.visibility = "hidden";
-        }
+      if (i == current) {
+        return;
       }
+
+      let pos;
+      let str;
+      let node;
+      let distance = Math.abs(underlines[i].getBoundingClientRect().right -
+                              underlines[current].getBoundingClientRect().right);
+      distance /= 2;
+
+      // fade out old underline
+      node = underlines[current].cloneNode(true);
+      underlines[current].parentNode.replaceChild(node, underlines[current]);
+
+      pos = current < i ? distance : distance * -1;
+      str = "translateX(" + pos + "px)";
+      node.animate([
+        {  // from
+          transform: 'translateX(0px)',
+          opacity: 1,
+          visibility: "visible",
+        },
+        {  // to
+          transform: str,
+          opacity: 0,
+          visibility: "hidden",
+        }
+      ], {
+        duration: 500,
+      });
+
+      // fade in new underline
+      node= underlines[i].cloneNode(true);
+      underlines[i].parentNode.replaceChild(node, underlines[i]);
+
+      pos = current < i ? distance * -1 : distance;
+      str = "translateX(" + pos + "px)";
+      node.animate([
+        {  // from
+          transform: str,
+          opacity: 0,
+          visibility: "hidden",
+        },
+        {  // to
+          transform: 'translateX(0px)',
+          opacity: 1,
+          visibility: "visible",
+        }
+      ], {
+        duration: 500,
+      });
+
+      underlines[i].style.visibility = "visible";
+      underlines[current].style.visibility = "hidden";
+      current = i;
     });
   }
 })();
