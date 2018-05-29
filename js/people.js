d7ef5525aadc1f78dc513d538417c2dc92453f23
hanchung <ghoo1125@gmail.com>
2018-05-29 16:38:05 +0800

    Add alumni slides and reconstruct slides layout.

--- js/people.js
+++ js/people.js
@@ -1,23 +1,51 @@
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function showDivs(index, slides) {
+  for (let i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+  }
+  slides[index - 1].style.display = "block";
 }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("people-slide");
-  if (n > x.length) {
-    slideIndex = 1;
+function getIndex(index, slides) {
+  if (index > slides.length) {
+    return 1;
   }
 
-  if (n < 1) {
-    slideIndex = x.length;
+  if (index < 1) {
+    return slides.length;
   }
 
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+  return index;
 }
+
+(function initSlide() {
+  let peopleIndex = 1;
+  let alumniIndex = 1;
+
+  let peopleSlides = document.getElementsByClassName("people-slide");
+  let alumniSlides = document.getElementsByClassName("alumni-slide");
+
+  let peopleLeftButton = document.getElementById("people-left-btn");
+  let peopleRightButton = document.getElementById("people-right-btn");
+  let alumniLeftButton = document.getElementById("alumni-left-btn");
+  let alumniRightButton = document.getElementById("alumni-right-btn");
+
+  showDivs(peopleIndex, peopleSlides);
+  showDivs(alumniIndex, alumniSlides);
+
+  peopleLeftButton.addEventListener("click", function() {
+    peopleIndex = getIndex(--peopleIndex, peopleSlides);
+    showDivs(peopleIndex, peopleSlides);
+  });
+  peopleRightButton.addEventListener("click", function() {
+    peopleIndex = getIndex(++peopleIndex, peopleSlides);
+    showDivs(peopleIndex, peopleSlides);
+  });
+  alumniLeftButton.addEventListener("click", function() {
+    alumniIndex = getIndex(--alumniIndex, alumniSlides);
+    showDivs(alumniIndex, alumniSlides);
+  });
+  alumniRightButton.addEventListener("click", function() {
+    alumniIndex = getIndex(++alumniIndex, alumniSlides);
+    showDivs(alumniIndex, alumniSlides);
+  });
+}) ();
